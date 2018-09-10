00a6c841a7ee5589061a7bce8fcaea6c78cd8a25
apecnascimento <apecnascimento@gmail.com>
2018-09-10 20:32:30 -0300

    Added separeted routes config.

--- videos/videos/__init__.py
+++ videos/videos/__init__.py
@@ -6,6 +6,7 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
+    config.include('.routes')
     config.add_static_view('static', 'static', cache_max_age=3600)    
     config.scan()
     return config.make_wsgi_app()
