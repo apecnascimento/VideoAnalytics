61710c26fb4b5785cbfcb45c547a8b526eaf294f
apecnascimento <apecnascimento@gmail.com>
2018-09-11 00:58:15 -0300

    Add Bootstrap and fontawesome.

--- videos/videos/views.py
+++ videos/videos/views.py
@@ -1,27 +1,27 @@
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPFound, 
+from pyramid.httpexceptions import HTTPFound 
 
 
-@view_config(route_name='videos', renderer='templates/videos/index.html', request_method='GET')
+@view_config(route_name='videos', renderer='templates/videos/index.jinja2', request_method='GET')
 def list_videos(request):
     videos = request.db_video_analytics.videos.find({}, {'_id': False})
     return {'videos': videos}
 
 
-@view_config(route_name='new_video', renderer='templates/videos/new.html', request_method='GET')
-def add_video(request):
-    return {}
+@view_config(route_name='new_video', renderer='templates/videos/new.jinja2', request_method='GET')
+def new_video(request):
+    return {'teste':''}
 
 
 @view_config(route_name='create_video', request_method='POST')
-def add_video(request):
+def create_video(request):
     name = request.params['name'].decode('utf-8')
     theme = request.params['theme'].decode('utf-8')
     request.db_video_analytics.insert_one({'name': name, 'theme': theme, 'thumbs_up':0, 'thumbs_down':0})
     return HTTPFound(request.route_url('videos'))
 
 @view_config(route_name='thumbs_up', request_method='POST')
-def add_video(request):
+def thumbs_up(request):
     video_name = request.matchdict.get('name', None)
     if video_name:
         video = request.db_video_analytics.videos.find_one({'name': name}, {'_id': False})
@@ -33,7 +33,7 @@ def add_video(request):
 
 
 @view_config(route_name='thumbs_down', request_method='POST')
-def add_video(request):
+def thumbs_down(request):
     video_name = request.matchdict.get('name', None)
     if video_name:
         video = request.db_video_analytics.videos.find_one({'name': name}, {'_id': False})
