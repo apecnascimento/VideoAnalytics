81f02f249c4d32b00e4d8a8d9f0af04adfe3a39f
apecnascimento <apecnascimento@gmail.com>
2018-09-10 22:44:39 -0300

    Added list_videos.

--- videos/videos/views.py
+++ videos/videos/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-def my_view(request):
-    return {'project': 'videos'}
+@view_config(route_name='videos', renderer='templates/videos/index.html', request_method='GET')
+def list_videos(request):
+    videos = request.db_video_analytics.videos.find({}, {'_id': False})
+    return {'videos': videos}
