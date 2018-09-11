def8b8b053659465810ca06d6b5f7124c1524f29
apecnascimento <apecnascimento@gmail.com>
2018-09-11 13:39:48 -0300

    Change videos view to root route.

--- videos/videos/routes.py
+++ videos/videos/routes.py
@@ -1,5 +1,5 @@
 def includeme(config):
-    config.add_route('videos', '/videos/')
+    config.add_route('videos', '/')
     config.add_route('new_video', '/videos/new/')
     config.add_route('create_video', '/videos/new/create/')
     config.add_route('thumbs_up', '/videos/thumbsup/{name}/')
