7690800af56c18b1bd18883ab09ae5e9d1424a75
apecnascimento <apecnascimento@gmail.com>
2018-09-10 23:34:36 -0300

    Added thumbs_up view.

--- videos/videos/views.py
+++ videos/videos/views.py
@@ -1,5 +1,5 @@
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPFound
+from pyramid.httpexceptions import HTTPFound, 
 
 
 @view_config(route_name='videos', renderer='templates/videos/index.html', request_method='GET')
@@ -19,4 +19,18 @@ def add_video(request):
     theme = request.params['theme'].decode('utf-8')
     request.db_video_analytics.insert_one({'name': name, 'theme': theme, 'thumbs_up':0, 'thumbs_down':0})
     return HTTPFound(request.route_url('videos'))
+
+@view_config(route_name='thumbs_up', request_method='POST')
+def add_video(request):
+    video_name = request.matchdict.get('name', None)
+    if video_name:
+        video = request.db_video_analytics.videos.find_one({'name': name}, {'_id': False})
+        if video:
+            thumbs_up = video['thumbs_up'] + 1
+            request.db_video_analytics.videos.update_one({'name': name}, {"$set":{"thumbs_up":thumbs_up}})
+            
+    return HTTPFound(request.route_url('videos'))
+
+
+
 
