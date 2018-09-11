47519fe7af7c36ecbe2fdf4402f9c52efa708057
apecnascimento <apecnascimento@gmail.com>
2018-09-10 23:18:26 -0300

    Added new and create views and routes.

--- videos/videos/routes.py
+++ videos/videos/routes.py
@@ -1,2 +1,4 @@
 def includeme(config):
     config.add_route('videos', '/videos/')
+    config.add_route('new_video', '/videos/new/')
+    config.add_route('create_video', '/videos/new/create/')
--- videos/videos/views.py
+++ videos/videos/views.py
@@ -1,7 +1,22 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 
 
 @view_config(route_name='videos', renderer='templates/videos/index.html', request_method='GET')
 def list_videos(request):
     videos = request.db_video_analytics.videos.find({}, {'_id': False})
     return {'videos': videos}
+
+
+@view_config(route_name='new_video', renderer='templates/videos/new.html', request_method='GET')
+def add_video(request):
+    return {}
+
+
+@view_config(route_name='create_video', request_method='POST')
+def add_video(request):
+    name = request.params['name'].decode('utf-8')
+    theme = request.params['theme'].decode('utf-8')
+    request.db_video_analytics.insert_one({'name': name, 'theme': theme})
+    return HTTPFound(request.route_url('videos'))
+
